575d9cc546e0d43748109efd0249f69ab02db8a9
Bijoy-cloud <bishalbijoy98@gmail.com>
2023-03-01 01:26:53 +0530

    origin removed

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.use(cors(
   // {credentials: true, origin: 'http://localhost:3001'}
 ));
 
+
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
   // create application/x-www-form-urlencoded parser
   var urlencodedParser = bodyParser.urlencoded({ extended: false })
   app.use(express.json({
