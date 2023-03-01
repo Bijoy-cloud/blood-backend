2e72b0002114c743e025c29549360a847086ab0c
Bijoy-cloud <bishalbijoy98@gmail.com>
2023-03-01 14:08:57 +0530

    Access-contro-origin added

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.use(cookieParser())
 
 
 app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+
   res.header("Access-Control-Allow-Origin", req.header('Origin'));
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
