988440dd4408fe565671a2b6dcbaa0dc463532d9
Bijoy-cloud <bishalbijoy98@gmail.com>
2023-03-01 21:58:12 +0530

    index.js changed

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ app.use(cookieParser())
 
 
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
 
-//   res.header("Access-Control-Allow-Origin", req.header('Origin'));
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+  res.header("Access-Control-Allow-Origin", req.header('Origin'));
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
   var urlencodedParser = bodyParser.urlencoded({ extended: false })
   app.use(express.json({
