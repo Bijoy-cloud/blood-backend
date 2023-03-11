9b917ec2844dab1a61e8f0042e487b17c678ea05
Bijoy-cloud <bishalbijoy98@gmail.com>
2023-03-12 00:25:09 +0530

    mysql connection

--- db.js
+++ db.js
@@ -1,22 +1,22 @@
 const mysql = require('mysql2')
-// const connection = mysql.createConnection({
-//   // host: 'localhost',
-//   // user: 'root',
-//   // password: 'BiJoY123@',
-//     host: 'sql12.freemysqlhosting.net',
-//     // port: 3306,
-//     user: 'sql12601058',
-//     password:"8JCAtSNE4K",
-//     database:'sql12601058'
-   
-// })
 const connection = mysql.createConnection({
-  
-  host:"containers-us-west-168.railway.app",
-  port:"6299",
-  user:"root",
-  password:"u1iq1SS13t9WVPZwrCvM",
- 
+  // host: 'localhost',
+  // user: 'root',
+  // password: 'BiJoY123@',
+    host: 'sql12.freemysqlhosting.net',
+    // port: 3306,
+    user: 'sql12601058',
+    password:"8JCAtSNE4K",
+    database:'sql12601058'
+   
 })
+// const connection = mysql.createConnection({
+  
+//   host:"containers-us-west-168.railway.app",
+//   port:"6299",
+//   user:"root",
+//   password:"u1iq1SS13t9WVPZwrCvM",
+ 
+// })
 
 module.exports = connection;    
